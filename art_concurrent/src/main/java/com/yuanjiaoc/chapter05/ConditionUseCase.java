baf2928c88395600b4e038e34d24e3cdd9bfe435
hejun <1761438882@qq.com>
2022-05-31 18:59:27 +0800

    ConcurrentPutHashMap

---
+++ art_concurrent/src/main/java/com/yuanjiaoc/chapter05/ConditionUseCase.java
@@ -0,0 +1,34 @@
+package com.yuanjiaoc.chapter05;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author 何二白
+ * @version 1.0
+ * @since 2022年05月31日
+ */
+public class ConditionUseCase {
+
+    Lock lock      = new ReentrantLock();
+    Condition condition = lock.newCondition();
+
+    public void conditionWait() throws InterruptedException {
+        lock.lock();
+        try {
+            condition.await();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void conditionSignal() throws InterruptedException {
+        lock.lock();
+        try {
+            condition.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
---
+++ art_concurrent/src/main/java/com/yuanjiaoc/chapter06/ConcurrentPutHashMap.java
@@ -0,0 +1,31 @@
+package com.yuanjiaoc.chapter06;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+/**
+ * @author 何二白
+ * @version 1.0
+ * @since 2022年05月31日
+ */
+public class ConcurrentPutHashMap {
+
+    public static void main(String[] args) throws InterruptedException {
+        final HashMap<String, String> map = new HashMap<String, String>(2);
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            map.put(UUID.randomUUID().toString(), "");
+                        }
+                    }, "ftf" + i).start();
+                }
+            }
+        }, "ftf");
+        t.start();
+        t.join();
+    }
+}
