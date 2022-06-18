e44d81ccdc97a8ccf90dd74943fdac07ca310c7f
hejun <1761438882@qq.com>
2022-06-18 10:00:05 +0800

    delegate

---
+++ spring/spring5-core-principle/src/main/java/com/yuanjiaoc/pattern/delegate/mvc/controllers/MemberController.java
@@ -0,0 +1,11 @@
+package com.yuanjiaoc.pattern.delegate.mvc.controllers;
+
+/**
+ * @author 何二白
+ * @version 1.0
+ * @since 2022年06月18日
+ */
+public class MemberController {
+
+  public void getMemberById(String mid) {}
+}
---
+++ spring/spring5-core-principle/src/main/java/com/yuanjiaoc/pattern/delegate/mvc/controllers/SystemController.java
@@ -0,0 +1,10 @@
+package com.yuanjiaoc.pattern.delegate.mvc.controllers;
+
+/**
+ * @author 何二白
+ * @version 1.0
+ * @since 2022年06月18日
+ */
+public class SystemController {
+  public void logout() {}
+}
