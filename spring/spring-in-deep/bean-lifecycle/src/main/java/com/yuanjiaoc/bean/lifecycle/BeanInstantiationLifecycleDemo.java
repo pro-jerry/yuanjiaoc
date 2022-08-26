95a08bafaa20502f78b8543f49668863c7fb86a6
pro-jerry <1761438882@qq.com>
2022-08-26 11:22:39 +0800

    userholder

--- spring/spring-in-deep/bean-lifecycle/src/main/java/com/yuanjiaoc/bean/lifecycle/BeanInstantiationLifecycleDemo.java
+++ spring/spring-in-deep/bean-lifecycle/src/main/java/com/yuanjiaoc/bean/lifecycle/BeanInstantiationLifecycleDemo.java
@@ -3,6 +3,7 @@ package com.yuanjiaoc.bean.lifecycle;
 import com.yuanjiaoc.common.domain.User;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Bean 实例化生命周期示例.
@@ -15,14 +16,65 @@ public class BeanInstantiationLifecycleDemo {
 
   public static void main(String[] args) {
 
-    executeBeanFactory();
+    //    executeBeanFactory();
+
+    //    executeBeanAware();
+    executeApplicationContext();
+  }
+
+  private static void executeBeanAware() {
+
+    DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+    // 方法一：添加 BeanPostProcessor 实现 MyInstantiationAwareBeanPostProcessor
+    beanFactory.addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
+    XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+    String[] locations = {
+      "META-INF/postProcessBeforeInstantiation.xml",
+      "META-INF/bean-constructor-dependency-injection.xml"
+    };
+    int beanNumbers = beanDefinitionReader.loadBeanDefinitions(locations);
+    System.out.println("已加载 BeanDefinition 数量：" + beanNumbers);
+    // 通过 Bean Id 和类型进行依赖查找
+    User user = beanFactory.getBean("user", User.class);
+    System.out.println(user);
+
+    User superUser = beanFactory.getBean("superUser", User.class);
+    System.out.println(superUser);
+
+    // 构造器注入按照类型注入，resolveDependency
+    UserHolder userHolder = beanFactory.getBean("userHolder", UserHolder.class);
+    System.out.println(userHolder);
+  }
+
+  private static void executeApplicationContext() {
+    ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext();
+    String[] locations = {
+      "META-INF/postProcessBeforeInstantiation.xml",
+      "META-INF/bean-constructor-dependency-injection.xml"
+    };
+    applicationContext.setConfigLocations(locations);
+    // 启动应用上下文
+    applicationContext.refresh();
+
+    User user = applicationContext.getBean("user", User.class);
+    System.out.println(user);
+
+    User superUser = applicationContext.getBean("superUser", User.class);
+    System.out.println(superUser);
+
+    // 构造器注入按照类型注入，resolveDependency
+    UserHolder userHolder = applicationContext.getBean("userHolder", UserHolder.class);
+    System.out.println(userHolder);
+
+    // 关闭应用上下文
+    applicationContext.close();
   }
 
   private static void executeBeanFactory() {
 
     DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
     // 方法一：添加 BeanPostProcessor 实现 MyInstantiationAwareBeanPostProcessor
-    //    beanFactory.addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
+    beanFactory.addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
     XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
     String[] locations = {
       "META-INF/postProcessBeforeInstantiation.xml",
--- spring/spring-in-deep/bean-lifecycle/src/main/java/com/yuanjiaoc/bean/lifecycle/UserHolder.java
+++ spring/spring-in-deep/bean-lifecycle/src/main/java/com/yuanjiaoc/bean/lifecycle/UserHolder.java
@@ -1,7 +1,16 @@
 package com.yuanjiaoc.bean.lifecycle;
 
+import javax.annotation.PostConstruct;
 import lombok.Data;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
 
 /**
@@ -12,7 +21,14 @@ import org.springframework.core.env.Environment;
  * @since 2022年08月22日
  */
 @Data
-public class UserHolder {
+public class UserHolder
+    implements BeanNameAware,
+        BeanClassLoaderAware,
+        BeanFactoryAware,
+        EnvironmentAware,
+        InitializingBean,
+        SmartInitializingSingleton,
+        DisposableBean {
 
   private User user;
 
@@ -34,11 +50,31 @@ public class UserHolder {
 
   public UserHolder() {}
 
+  @PostConstruct
+  public void initPostConstruct() {
+    // postProcessBeforeInitialization V3 -> initPostConstruct V4
+    this.description = "The user holder V4";
+    System.out.println("initPostConstruct() = " + description);
+  }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    this.description = "The user holder V5";
+    System.out.println("afterPropertiesSet() = " + description);
+  }
+
   /** 自定义初始化方法 */
   public void init() {
     // initPostConstruct V5 -> afterPropertiesSet V6
     this.description = "The user holder V6";
     System.out.println("init() = " + description);
+  }
+
+  @Override
+  public void afterSingletonsInstantiated() {
+    // postProcessAfterInitialization V7 -> afterSingletonsInstantiated V8
+    this.description = "The user holder V8";
+    System.out.println("afterSingletonsInstantiated() = " + description);
   }
 
   public void destroy() throws Exception {
@@ -51,5 +87,34 @@ public class UserHolder {
     // destroy : The user holder V11
     this.description = "The user holder V12";
     System.out.println("doDestroy() = " + description);
+  }
+
+  public String toString() {
+    return "UserHolder{"
+        + "user="
+        + user
+        + ", number="
+        + number
+        + ", description='"
+        + description
+        + '\''
+        + ", beanName='"
+        + beanName
+        + '\''
+        + '}';
+  }
+
+  public void setBeanClassLoader(ClassLoader classLoader) {
+    this.classLoader = classLoader;
+  }
+
+  @Override
+  public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+    this.beanFactory = beanFactory;
+  }
+
+  @Override
+  public void setBeanName(String name) {
+    this.beanName = name;
   }
 }
