bcd144230351d6d75ac5465e92ce6816c7dd4e5e
pro-jerry <1761438882@qq.com>
2022-09-20 17:54:59 +0800

    ConcurrentHashMap

---
+++ spring/spring-hello/src/test/java/CHMDemo.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author 何二白
+ * @version 1.0
+ * @since 2022年09月20日
+ */
+public class CHMDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        ConcurrentHashMap<String, AtomicInteger> map = new ConcurrentHashMap<String, AtomicInteger>();
+        AtomicInteger integer = new AtomicInteger(1);
+        map.put("key", integer);
+        ExecutorService executorService = Executors.newFixedThreadPool(1000);
+        for (int i = 0; i < 1000; i++) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    int key = map.get("key").incrementAndGet();
+                }
+            });
+        }
+        Thread.sleep(3000); //模拟等待执行结束
+        System.out.println("------" + map.get("key") + "------");
+        executorService.shutdown();
+    }
+}
